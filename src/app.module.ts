250620d81ef21d950c7ab8aa749e8f15f2d04c40
Luiz Carlos <junior1232131@gmail.com>
2025-11-19 16:39:19 -0300

    chore: generate modules by CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EmailService } from './providers/email/email.service';
+import { PrismaModule } from './providers/prisma/prisma.module';
+import { EmailModule } from './providers/email/email.module';
+import { UsersModule } from './modules/users/users.module';
+import { PreferencesModule } from './modules/preferences/preferences.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { EventsModule } from './modules/events/events.module';
+import { SchedulerModule } from './modules/scheduler/scheduler.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, EmailModule, UsersModule, PreferencesModule, NotificationsModule, EventsModule, SchedulerModule, SharedModule],
+  controllers: [],
+  providers: [EmailService],
 })
 export class AppModule {}
